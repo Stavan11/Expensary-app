fec80402b5315f3885a6e28c0c17c4340edae342
Stavan Panhalkar <stavan.panhalkar@gmail.com>
2024-06-27 00:10:08 +0530

    No need of the build folder anymore

--- build.py
+++ build.py
@@ -1,19 +1,29 @@
-import cx_Freeze
-import sys
 from cx_Freeze import setup, Executable
+import os
 
+# Define the output directory
+output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "ExpenSary Version1")
+
+# Define the build options
+build_exe_options = {
+    "packages": ["tkinter", "os", "pandas", "categorize_expenses"],
+    "include_files": [],
+    "build_exe": output_dir
+}
+
+# Define the base
 base = None
+if os.name == 'nt':
+    base = 'Win32GUI'  # Use 'Win32GUI' to hide the console window (only for Windows)
 
-# Only use the base parameter for Windows systems
-if sys.platform == "win32":
-    base = "Win32GUI"
+# Define the executable
+executables = [Executable("gui.py", base=base)]
 
-# Define the setup parameters
+# Setup the cx_Freeze
 setup(
     name="Expensary",
     version="1.0",
     description="Expenditure Summary Generator",
-    options={"build_exe": {"packages": ["tkinter", "os", "pandas", "categorize_expenses"],
-                           "include_files": []}},
-    executables=[Executable("gui.py", base=base)]
+    options={"build_exe": build_exe_options},
+    executables=executables
 )
